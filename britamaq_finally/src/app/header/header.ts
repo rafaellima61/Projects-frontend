54d2917d68191bf2d2d3999e0b4cb44845576d1d
rafaeldev1 <rafaellima61app@gmail.com>
2025-06-02 15:11:14 -0300

    component header created

---
+++ britamaq_finally/src/app/header/header.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+  // Header component logic can be added here if needed
+  title = 'My Application Header';
+
+}
